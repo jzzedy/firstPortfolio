aa403560e252dbd2383396b81857a4fc4fd93a59
Zed <102397706+jzzedy@users.noreply.github.com>
2022-04-11 17:39:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ const sr =ScrollReveal ({
 
 sr.reveal('.home-text',{delay: 200, origin: 'top'})
 sr.reveal('.home-img',{delay: 400, origin: 'top'})
-sr.reveal('.about, .skills, .quest, .education, education-content, .contact, .rights, .received', {delay: 200, origin: 'top'})
+sr.reveal('.about, .skills, .quest, .education, education-content, .contact, .rights, .receive', {delay: 200, origin: 'top'})
